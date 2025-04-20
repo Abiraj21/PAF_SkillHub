9596e3b25c433f7b2036cb88fbc3f589fa315ac4
IT21118654 <it21118654@my.sliit.lk>
2025-04-20 12:21:07 +0530

    Updated Files
    Updated the Readme and file structure

---
+++ Backend/src/main/java/com/Recipe_Management/Recipe_Management/model/User.java
@@ -0,0 +1,117 @@
+package com.Recipe_Management.Recipe_Management.model;
+import com.Recipe_Management.Recipe_Management.enums.AuthProvider;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name="User")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private String role;
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Enumerated(EnumType.STRING)
+    private AuthProvider authProvider;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public AuthProvider getAuthProvider() {
+        return authProvider;
+    }
+
+    public void setAuthProvider(AuthProvider authProvider) {
+        this.authProvider = authProvider;
+    }
+
+}
---
+++ Readme.txt
@@ -0,0 +1,20 @@
+PAF_Prorata_02
+
+Members.
+
+IT21118654 : Kailashwaran R.A.K.K - 
+IT21XXXXXX
+IT21XXXXXX
+IT21XXXXXX
+
+User Authentication 0Auth and User Registration functions Contributed by all 4 member.
+
+Social Media Application - SkillHub💡
+
+The project involves designing and implementing a Skill-Sharing & Learning Platform that allows users to share and learn various skills, cooking, recipe management, and Sharing Cooking methods. The system will consist of a Java Spring Boot REST API and a React-based client web application.
+
+Tools Used for the Application
+
+Frontend - Reactjs
+Backend  - SpringBoot
+Database - MYSQL Database
