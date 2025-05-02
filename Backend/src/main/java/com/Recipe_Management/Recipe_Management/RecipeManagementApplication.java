519af46b786bd55e7ad66b5b6f8721dab0a94203
IT21118654 <it21118654@my.sliit.lk>
2025-05-02 23:48:19 +0530

    Application Class
    Application class for running the program is implemented

---
+++ Backend/src/main/java/com/Recipe_Management/Recipe_Management/RecipeManagementApplication.java
@@ -0,0 +1,17 @@
+package com.Recipe_Management.Recipe_Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication()
+
+public class RecipeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeManagementApplication.class, args);
+	}
+
+}
