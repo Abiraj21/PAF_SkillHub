c502fa2ca27e99acea746e838f69e813da07fdfb
Bingun Alahakoon <68630339+IT19108346@users.noreply.github.com>
2025-05-08 00:08:15 +0530

    Add_Meal_Plan_Update_Endpoint_Implement_Put_/meal/update/{id}_Endpoint_To_Update_An_Existing_Meal_Plan_Using_Mealplandto.

--- Backend/src/main/java/com/Recipe_Management/Recipe_Management/controller/MealPlanController.java
+++ Backend/src/main/java/com/Recipe_Management/Recipe_Management/controller/MealPlanController.java
@@ -3,7 +3,9 @@ package com.Recipe_Management.Recipe_Management.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +30,11 @@ public class MealPlanController {
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<MealPlan> updateMealPlan(@RequestBody MealPlanDTO dto, @PathVariable Long id) {
+        return ResponseEntity.ok(mealPlanService.updateMeal(id, dto));
     }
 }
 
