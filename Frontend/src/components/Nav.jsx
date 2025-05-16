bd10164bcd5d16c5f4c1dcacdac5bf0d5bf5f60f
IT21118654 <it21118654@my.sliit.lk>
2025-05-16 11:23:01 +0530

    Created the navigation bar
    Navigation bar component added

---
+++ Frontend/src/components/Nav.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faBell, faEnvelope, faHome, faSearch,faUser,faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import Logo from '../assets/logo2.jpg'
+
+export default function Nav(){
+    return (
+        <nav className="bg-white shadow-md px-4 py-5">
+            <div className="max-w-7xl mx-auto flex justify-between items-center">
+                {/* Left Side */}
+                <div className="flex items-center space-x-9">
+                    {/* Logo */}
+                    <Link to="/profile" onClick={() => {
+                                            localStorage.setItem("token", null);
+                                            localStorage.setItem("id", null);
+                                        }}>
+                        <img src={Logo} alt="Skillhub logo" className="w-30 h-auto float-left " />
+                    </Link>
+
+                    {/* Icons */}
+                    <Link to="/profile">
+                        <FontAwesomeIcon icon={faUser} className="text-xl text-gray-600 hover:text-blue-500" />
+                    </Link>
+
+                    {/* Search Bar */}
+                    <div className="flex items-center border border-gray-300 rounded-full px-3 py-1">
+                        <FontAwesomeIcon icon={faSearch} className="text-gray-500 mr-2" />
+                        <input
+                                type="search"
+                                placeholder="Search..."
+                                className="outline-none text-sm w-full"
+                        />
+                    </div>
+                </div>
+
+                {/* Right Side */}
+                <div className="flex items-center space-x-6">
+                    <Link to="">
+                        <FontAwesomeIcon icon={faEnvelope} className="text-xl text-gray-600 hover:text-blue-500" />
+                    </Link>
+                    <Link to="">
+                        <FontAwesomeIcon icon={faBell} className="text-xl text-gray-600 hover:text-blue-500" />
+                    </Link>
+                    <Link to="/" onClick={() => {
+                                            localStorage.setItem("token", null);
+                                            localStorage.setItem("id", null);
+                                        }}>
+                        <FontAwesomeIcon icon={faRightFromBracket } className="text-xl text-gray-600 hover:text-blue-500" />
+                    </Link>
+                </div>
+            </div>
+        </nav>
+
+    )
+}
