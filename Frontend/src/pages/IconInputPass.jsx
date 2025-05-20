67f5e7e470474854254f2771563fc7dbf6fa7e7e
IT21118654 <it21118654@my.sliit.lk>
2025-05-20 13:03:14 +0530

    email pass frontend
    added some styling for email and password

---
+++ Frontend/src/pages/IconInputPass.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function IconInputPass({children,placeholder,text,value, onChange}){
+  
+    return(
+      <div className='flex justify-left items-center w-full h-10 shadow mt-4'>
+        <div className='icon_wrapper flex absolute w-14 justify-center items-center'>
+          <span className='text-gray-500 opacity-80 text-xl '>{children}</span>
+        </div>
+        <input type={text} placeholder={placeholder} className='w-full h-full pl-14' value={value} onChange={onChange}/>
+      </div>
+    )
+  }
